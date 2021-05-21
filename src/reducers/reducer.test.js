d03c6603a96551e1b58331704a4faa98c309f6a2
stasvol <https://github.com/stasvol/my-test.git>
2021-05-21 21:24:04 +0300

    update file

--- src/reducers/reducer.test.js
+++ src/reducers/reducer.test.js
@@ -3,47 +3,47 @@ import {SET_TABLE, setTableSize} from "./set_table";
 import {ADD_USERS, usersReduc} from "./users";
 
 
-describe('testing reducers', ()=>{
+describe('testing reducers', () => {
 
-    it('setDataRedux ',  ()=> {
-    const state={
-       data: null
-    }
-    const action ={
-        type:SET_DATA,
-        payload: [1,2,3]
-    }
-        expect(setDataRedux(state,action)).toEqual({
+    it('setDataRedux ', () => {
+        const state = {
+            data: null
+        }
+        const action = {
+            type: SET_DATA,
+            payload: [1, 2, 3]
+        }
+        expect(setDataRedux(state, action)).toEqual({
             // ...state,
             data: action.payload
         })
     });
 
     it('setTableSize', function () {
-        const state={
+        const state = {
             // pageSize: 3
         }
-        const action={
+        const action = {
             type: SET_TABLE,
             payload: 9
         }
 
-        expect(setTableSize(state,action)).toEqual({
+        expect(setTableSize(state, action)).toEqual({
             currentPage: action.payload
         })
     });
 
     it('usersReduc', function () {
-        const state ={}
+        const state = {}
 
-        const action={
+        const action = {
             type: ADD_USERS,
-            payload:['atos','roma','don']
+            payload: ['atos', 'roma', 'don']
         }
 
-        expect(usersReduc(state,action)).toEqual({
+        expect(usersReduc(state, action)).toEqual({
             // ...state,
-            users:action.payload
+            users: action.payload
         })
     });
 
